2473890f3073bc6fabd51a2bdafddd81a40683bd
longtianaowei <1162941555@qq.com>
2024-06-12 16:46:58 +0800

    顺序表的动态分配

--- 顺序表动态分配.c
+++ 顺序表动态分配.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 10 // 顺序表的最大容量
-
 // 定义顺序表结构
 typedef struct {
-    int data[MAX_SIZE]; // 存放顺序表的元素
-    int length;         // 顺序表的当前长度
+    int *data;  // 存放顺序表的元素
+    int length; // 顺序表的当前长度
+    int capacity; // 顺序表的最大容量
 } SeqList;
 
 // 初始化顺序表
-void InitList(SeqList *list) {
-    for (int i = 0; i < MAX_SIZE; i++)
-    {
-        list->data[i] = 0; //将所有数据元素设置为默认初始值
+void InitList(SeqList *list, int initialSize) {
+    list->data = (int *)malloc(initialSize * sizeof(int));
+    if (list->data == NULL) {
+        printf("内存分配失败。\n");
+        exit(1);
     }
-    
-    list->length = 0; // 初始长度为0
+    list->length = 0;
+    list->capacity = initialSize;
 }
 
 // 判断顺序表是否为空
@@ -26,18 +26,29 @@ int IsEmpty(SeqList list) {
 
 // 判断顺序表是否已满
 int IsFull(SeqList list) {
-    return list.length == MAX_SIZE;
+    return list.length == list.capacity;
 }
 
-// 向顺序表中插入元素  positon为具体的位置从1开始，不是数组下标从0开始  (增)
-int Insert(SeqList *list, int position, int element) {
-    if (IsFull(*list)) {
-        printf("顺序表已满，无法插入元素。\n");
-        return 0;
+// 扩展顺序表的容量
+void ExpandList(SeqList *list) {
+    int newCapacity = list->capacity * 2;
+    int *newData = (int *)realloc(list->data, newCapacity * sizeof(int));
+    if (newData == NULL) {
+        printf("内存重新分配失败。\n");
+        exit(1);
     }
+    list->data = newData;
+    list->capacity = newCapacity;
+}
+
+// 向顺序表中插入元素
+int Insert(SeqList *list, int position, int element) {
     if (position-1 < 0 || position-1 > list->length) {
         printf("插入位置不合法。\n");
         return 0;
+    }
+    if (IsFull(*list)) {
+        ExpandList(list);
     }
     // 将插入位置之后的元素后移
     for (int i = list->length; i > position-1; i--) {
@@ -48,7 +59,7 @@ int Insert(SeqList *list, int position, int element) {
     return 1;
 }
 
-// 删除顺序表中的元素 (删)
+// 删除顺序表中的元素
 int Delete(SeqList *list, int position) {
     if (IsEmpty(*list)) {
         printf("顺序表为空，无法删除元素。\n");
@@ -66,7 +77,7 @@ int Delete(SeqList *list, int position) {
     return 1;
 }
 
-// 按值查找顺序表中的元素 （查）
+// 按查找顺序表中的元素
 int Find(SeqList list, int element) {
     for (int i = 0; i < list.length; i++) {
         if (list.data[i] == element) {
@@ -96,9 +107,17 @@ void PrintList(SeqList list) {
     printf("\n");
 }
 
+// 释放顺序表的内存
+void FreeList(SeqList *list) {
+    free(list->data);
+    list->data = NULL;
+    list->length = 0;
+    list->capacity = 0;
+}
+
 int main() {
     SeqList list;
-    InitList(&list);
+    InitList(&list, 5);
 
     Insert(&list, 1, 10);
     Insert(&list, 2, 20);
@@ -114,6 +133,7 @@ int main() {
     } else {
         printf("元素30不在顺序表中。\n");
     }
+
     int upposition = Update(&list,30,33);
      if (upposition != -1) {
         printf("元素30的已被更新,新的顺序表为");
@@ -121,6 +141,8 @@ int main() {
     } else {
         printf("元素30不在顺序表中。\n");
     }
+
+    FreeList(&list);
 
     return 0;
 }
